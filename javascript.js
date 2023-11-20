1ea7595505f949c3a51d7180924872feae0dcd91
evinsdesign <kelvinsaputra024@gmail.com>
2023-11-20 20:41:45 +0800

    javascript onSubmit, addTodo

---
+++ javascript.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const submitForm = document.getElementById('form');
+    
+    // onSubmit
+    submitForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        addTodo();
+    });
+    function addTodo(){
+        const textTodo = document.getElementById('title').value;
+        const timeStamp = document.getElementById('date').value;
+
+        const generateID = generateId();
+        const todoObject  = generateTodoObject(generateID, textTodo, timeStamp, false);
+        todos.push(todoObject);
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    };
+    function generateId(){
+        return +new Date();
+    };
+    function generateTodoObject(id, task, timestamp, isComplated){
+        return{
+            id,
+            task,
+            timestamp,
+            isComplated
+        }
+    }
+});
